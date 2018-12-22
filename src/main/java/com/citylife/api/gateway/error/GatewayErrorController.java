8d2c2c9f12f45eeb9c56f14265638f8f1f4096aa
AzureAD\MingLiangChen <cmliang@cn.ibm.com>
2018-12-23 00:29:35 +0800

    update

--- src/main/java/com/citylife/api/gateway/error/GatewayErrorController.java
+++ src/main/java/com/citylife/api/gateway/error/GatewayErrorController.java
@@ -1,15 +1,17 @@
 package com.citylife.api.gateway.error;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.citylife.api.gateway.http.client.bean.JsonResponseBean;
-
-import javax.servlet.http.HttpServletRequest;
+import com.citylife.common.component.TraceHelper;
+import com.citylife.common.constants.SystemMessageConsts;
+import com.citylife.common.message.MessageResolver;
+import com.citylife.common.model.ResultEntity;
 
 /**
  * 自定义异常错误处理器
@@ -18,19 +20,27 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class GatewayErrorController implements ErrorController {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+  public GatewayErrorController() {
+    super();
+  }
 
-    @Override
-    public String getErrorPath() {
-        return "/gateway/error";
-    }
+  @Autowired
+  private MessageResolver messageResolver;
 
-    @GetMapping("/error")
-    public JsonResponseBean error(HttpServletRequest request) {
-        if (logger.isErrorEnabled()) {
-            logger.error("网关层发生未知异常");
-        }
-        return JsonResponseBean.buildCcfResponseBean(JsonResponseBean.GW_INTERNAL_ERROR_CODE, "系统内部异常, 请通知系统管理员进行排查.");
-    }
+  @Autowired
+  private TraceHelper traceHelper;
+
+  @Override
+  public String getErrorPath() {
+    return "/gateway/error";
+  }
+
+  @RequestMapping(method = { RequestMethod.GET, RequestMethod.POST }, value = "/error")
+  public ResultEntity<?> error(HttpServletRequest request) {
+    ResultEntity<?> result = ResultEntity.failure(ResultEntity.SYSTEM_ERROR,
+        messageResolver.error(SystemMessageConsts.SYSTEM_ERROR_UNEXPECTED));
+    result.setTraceId(traceHelper.getTraceId(request));
+    return result;
+  }
 
 }
---
+++ src/main/java/com/citylife/api/gateway/error/ZuulErrorFilter.java
@@ -0,0 +1,39 @@
+package com.citylife.api.gateway.error;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import com.citylife.common.logging.AdminLogger;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ZuulErrorFilter extends ZuulFilter {
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() throws ZuulException {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    Throwable throwable = ctx.getThrowable();
+    AdminLogger.error("this is a ErrorFilter :" + throwable.getCause().getMessage(), throwable);
+    ctx.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    ctx.set("error.exception", throwable.getCause());
+    return null;
+  }
+
+  @Override
+  public String filterType() {
+    return FilterConstants.ERROR_TYPE;
+  }
+
+  @Override
+  public int filterOrder() {
+    return 10;
+  }
+
+}
